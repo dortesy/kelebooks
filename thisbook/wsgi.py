e195305b510a2ebd27c8a5219e54e9b6b41cbeee
Sherzod Saidmaksumov <dortesdortes@gmail.com>
2023-12-28 15:45:30 -0500

    gs

--- thisbook/wsgi.py
+++ thisbook/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kelebooks.com.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thisbook.settings')
 
 application = get_wsgi_application()
